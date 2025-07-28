9e10567d119ec55b386da93dc995f31e45d2b3ac
mash3al-29 <abdelrahmanmashaal@gmail.com>
2025-07-28 15:14:58 +0300

    refactor: Refactored the whole codebase to follow best practices in themes, fonts, icons, shapes and strings. Also, Added see all screen as needed

--- app/src/main/java/com/mashaal/ecommerce_app/ui/theme/Type.kt
+++ app/src/main/java/com/mashaal/ecommerce_app/ui/theme/Type.kt
@@ -1,11 +1,7 @@
 package com.mashaal.ecommerce_app.ui.theme
 
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import com.mashaal.ecommerce_app.R
 
 
@@ -19,41 +15,4 @@ val GilroyMediumFont = FontFamily(
 
 val GilroyRegularFont = FontFamily(
     Font(R.font.gilroy_regular)
-)
-val Typography = Typography(
-    titleMedium = TextStyle(
-        fontFamily = GilroyBoldFont,
-        fontWeight = FontWeight.W400,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.15.sp
-    ),
-    bodySmall = TextStyle(
-        fontFamily = GilroyMediumFont,
-        fontWeight = FontWeight.W400,
-        fontSize = 14.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 0.25.sp
-    ),
-    bodyLarge = TextStyle(
-        fontFamily = GilroyRegularFont,
-        fontWeight = FontWeight.W600,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    ),
-    titleLarge = TextStyle(
-        fontFamily = GilroyRegularFont,
-        fontWeight = FontWeight.W600,
-        fontSize = 20.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = GilroyRegularFont,
-        fontWeight = FontWeight.W600,
-        fontSize = 14.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 0.25.sp
-    )
 )
